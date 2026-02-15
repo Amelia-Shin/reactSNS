5478a7331879f907fe0243f382d7a0ca4b667c51
Amelia-Shin <gmldnjs0402@naver.com>
2026-02-15 16:57:19 +0900

    feat: OAuth 로그인 구현 (github)

--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -1,21 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in-with-password";
-import { useSignUp } from "@/hooks/mutations/use-sign-up";
 import { useState } from "react";
 import { Link } from "react-router";
+
+import gitHubLogo from "@/assets/github-mark.svg";
+import { useSignInWithOAuth } from "@/hooks/mutations/use-sign-in-with-oauth";
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { mutate: signInWithPassword } = useSignInWithPassword();
+  const { mutate: signInWithOAuth } = useSignInWithOAuth();
 
   const handelSignInWithPassword = () => {
     if (email.trim() === "") return;
     if (password.trim() === "") return;
 
     signInWithPassword({ email, password });
+  };
+
+  const handelSignInWithOAuth = () => {
+    signInWithOAuth("github");
   };
 
   return (
@@ -37,9 +44,17 @@ export default function SignInPage() {
           placeholder="password"
         />
       </div>
-      <div>
+      <div className="flex flex-col gap-2">
         <Button onClick={handelSignInWithPassword} className="w-full">
           로그인
+        </Button>
+        <Button
+          onClick={handelSignInWithOAuth}
+          className="w-full"
+          variant={"outline"}
+        >
+          <img src={gitHubLogo} className="h-4 w-4" />
+          GitHub 계정으로 로그인하기
         </Button>
       </div>
       <div>
