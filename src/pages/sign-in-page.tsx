5a8fddc345ef87022db4375f27800ababbd3b401
Amelia-Shin <gmldnjs0402@naver.com>
2026-02-15 22:17:15 +0900

    feat: 인증 에러처리(onError)

--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -6,12 +6,20 @@ import { Link } from "react-router";
 
 import gitHubLogo from "@/assets/github-mark.svg";
 import { useSignInWithOAuth } from "@/hooks/mutations/use-sign-in-with-oauth";
+import { toast } from "sonner";
 
 export default function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const { mutate: signInWithPassword } = useSignInWithPassword();
+  const { mutate: signInWithPassword } = useSignInWithPassword({
+    onError: (error) => {
+      toast.error(error.message, {
+        position: "top-center",
+      });
+      setPassword("");
+    },
+  });
   const { mutate: signInWithOAuth } = useSignInWithOAuth();
 
   const handelSignInWithPassword = () => {
