b13814958eccd530de41e6d748c567b3dd624f5b
Amelia-Shin <gmldnjs0402@naver.com>
2026-02-24 22:44:52 +0900

    feat: 포스트 조회 기능 추가

---
+++ src/components/post/post-feed.tsx
@@ -0,0 +1,19 @@
+import { usePostsData } from "@/hooks/queries/use-post-data";
+import Fallback from "../fallback";
+import Loader from "../loader";
+import PostItem from "./post-item";
+
+export default function PostFeed() {
+  const { data, error, isPending } = usePostsData();
+
+  if (error) return <Fallback />;
+  if (isPending) return <Loader />;
+
+  return (
+    <div>
+      {data.map((post) => (
+        <PostItem key={post.id} {...post} />
+      ))}
+    </div>
+  );
+}
