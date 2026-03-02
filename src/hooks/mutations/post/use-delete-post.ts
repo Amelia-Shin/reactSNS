427bd4f7d9691aa6c5043366ca685a31ab3b36c3
Amelia-Shin <gmldnjs0402@naver.com>
2026-03-02 17:21:01 +0900

    feat: 포스트 추가/수정/삭제 캐싱 기능 추가

--- src/hooks/mutations/post/use-delete-post.ts
+++ src/hooks/mutations/post/use-delete-post.ts
@@ -1,9 +1,12 @@
 import { deleteImages } from "@/api/image";
 import { deletePost } from "@/api/post";
+import { QUERY_KEYS } from "@/lib/constants";
 import type { useMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeletePost(callbacks?: useMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deletePost,
     onSuccess: async (deletedPost) => {
@@ -14,6 +17,8 @@ export function useDeletePost(callbacks?: useMutationCallback) {
         const paths = imageUrls.map((url) => url.split("/uploads/")[1]);
         await deleteImages(paths);
       }
+
+      queryClient.resetQueries({ queryKey: QUERY_KEYS.post.list });
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
