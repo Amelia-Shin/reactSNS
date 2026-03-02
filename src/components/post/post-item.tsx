bf801e2cf279b1097ef6f4c6e208be68b247fa6a
Amelia-Shin <gmldnjs0402@naver.com>
2026-03-02 15:52:57 +0900

    feat: 수정/삭제 버튼 내 포스트에만 렌더링

--- src/components/post/post-item.tsx
+++ src/components/post/post-item.tsx
@@ -9,8 +9,14 @@ import {
 import { formatTimeAgo } from "@/lib/time";
 import { EditPostButton } from "./edit-post-button";
 import { DeletetPostButton } from "./delete-post-button";
+import { useSession } from "@/store/session";
 
 export default function PostItem(post: Post) {
+  const session = useSession();
+  const userId = session?.user.id;
+
+  const isMine = post.author_id === userId;
+
   return (
     <div className="flex flex-col gap-4 border-b pb-8">
       {/* 1. 유저 정보, 수정/삭제 버튼 */}
@@ -33,9 +39,14 @@ export default function PostItem(post: Post) {
         </div>
 
         {/* 1-2. 수정/삭제 버튼 */}
+
         <div className="text-muted-foreground flex text-sm">
-          <EditPostButton {...post} />
-          <DeletetPostButton id={post.id} />
+          {isMine && (
+            <>
+              <EditPostButton {...post} />
+              <DeletetPostButton id={post.id} />
+            </>
+          )}
         </div>
       </div>
 
